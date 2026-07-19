657cdb57b3fc1fb2d89c852f74ca42afc08f0550
areebbaig580 <areebbaig580@gmail.com>
2026-07-19 22:56:05 +0530

    Change to show more data

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -11,27 +11,20 @@ const Discover = ({ genre, setShowId }) => {
   useEffect(() => {
     const fetchData = async () => {
 
-      const URL = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=${genre}`
-      const response = await fetch(URL);
-      const data = await response.json();
-      console.log(data);
-      let movieData = [];
+      const pages = await Promise.all(
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(page =>
+          fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=${genre}&page=${page}`)
+            .then(res => res.json())
+        )
+      );
 
-      let resultLen = data.results.length;
+      const movieData = pages.flatMap(data =>
+        data.results
+          .filter(m => m.poster_path)
+          .map(m => ({ name: m.title, poster: `${posterUrl}${m.poster_path}`, id: m.id }))
+      );
 
-      for (let i = 0; i < resultLen; i++) {
-        let card = {};
-        let title = data.results[i].title;
-        let posterPath = data.results[i].poster_path;
-        if (posterPath === null || posterPath === undefined) continue;
-        let poster = `${posterUrl}${posterPath}`
-        let id = data.results[i].id;
-
-        card = { name: title, poster: poster, id: id };
-        movieData.push(card);
-      }
       setMovies(movieData);
-
       setLoading(false);
     }
     fetchData();
@@ -41,8 +34,8 @@ const Discover = ({ genre, setShowId }) => {
     <div className='h-fit w-full flex flex-wrap py-4 bg-[#131313] md:gap-5 justify-center gap-3 px-1 md:px-2'>
       {movies.map((e, index) => (
 
-        <div className='w-[130px] mb-2 md:w-[160px]' key={index}>
-          <img src={e.poster} alt="" className='h-[28vh] md:h-[34vh] w-full object-contain rounded-lg' />
+        <div className='w-[100px] mb-2 md:w-[160px]' key={index}>
+          <img src={e.poster} alt="" className='h-[18vh] md:h-[34vh] w-full object-contain rounded-lg' />
           <div className='bg-[#191919] rounded-b-lg min-h-[12vh] w-full pl-2 py-2'>
             <div>{e.name}</div>
             <Link className=' text-amber-300 text-sm cursor-pointer mt-2'
